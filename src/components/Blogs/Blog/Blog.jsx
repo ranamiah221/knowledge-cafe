d45d7fba5d014b71e5351fa9097a05b52fd1d1a7
ranamiah221 <ranarasul21@gmail.com>
2024-09-22 20:39:12 +0600

    added Header element

--- src/components/Blogs/Blog/Blog.jsx
+++ src/components/Blogs/Blog/Blog.jsx
@@ -18,10 +18,15 @@ const Blog = ({ blog }) => {
       </figure>
       <div className="card-body">
         <h2 className="card-title">{title}</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
-        </div>
+         <div>
+            <div>
+                <img src={author_img} alt="" />
+                <h2>{author}</h2>
+            </div>
+            <div>
+                  
+            </div>
+         </div>
       </div>
     </div>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
-
+import profile from'../../assets/IMG-20240222-WA0003_1.jpg'
 const Header = () => {
     return (
-        <div>
-            <h2 className="text-4xl font-medium bg-gray-400">knowledge cafe</h2>
+        <div className='flex justify-between border-b-2 my-2 py-2'>
+            <h2 className="text-4xl font-medium text-gray-600">Knowledge Cafe</h2>
+            <img className='h-20 w-20 rounded-full' src={profile} alt="" />
         </div>
     );
 };
