8ea1493f7c0c9b6e61ef82244ed2126d29007b2b
ranamiah221 <ranarasul21@gmail.com>
2024-09-22 11:33:48 +0600

    set up a react project with tailwind css project name is knowledge-cafe

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h2 className='text-2xl font-medium bg-red-300'>knowledge cafe</h2>
+      
+    </>
+  )
+}
+
+export default App
